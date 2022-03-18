702c05ae48edc5cf1101ae82fe54a1be3afba48f
Tony Chao <tchao@mail.usf.edu>
2022-03-17 22:16:16 -0400

    Refactor toDoList web app to include Mongodb and render db items
    Added Mongodb to store the item data sent from toDoList web app.
    
    When the main route is loaded up for the first time, create the default items in Mongodb and redirect user back to home page to display the default items.
    
    Otherwise, render the home page using EJS with the current date and all the data stored for the Item collection.

--- app.js
+++ app.js
@@ -1,22 +1,71 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js"); // Export a local module from date.js
 
 const app = express();
 
-let items = ["Study"];
-let workItems = [];
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
-app.get("/", function (req, res) {
-    let day = date.getCurrentDate();
+// Connect to Local MongoDB
+mongoose.connect("mongodb://localhost:27017/toDoListDB");
 
-    res.render("list", {
-        listTitle: day,
-        newListItems: items
+// Items Schema
+const itemsSchema = {
+    name: String,
+    date: Date
+};
+
+// Create the Items Collection
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your toDoList!",
+    date: new Date()
+});
+
+const item2 = new Item({
+    name: "Click on (+) sign to add a new item",
+    date: new Date()
+});
+
+const item3 = new Item({
+    name: "Click on (x) to delete a existing item",
+    date: new Date()
+});
+
+const defaultItems = [item1, item2, item3];
+
+// Lists Schema
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+// Create the Lists Collection
+const List = mongoose.model("List", listSchema);
+
+app.get("/", function (req, res) {
+    Item.find({}, function(err, foundItems) {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully inserted default items.")
+                }
+            });
+
+            res.redirect("/");
+        } else {
+            res.render("list", {
+                listTitle: date.getCurrentDate(),
+                newListItems: foundItems
+            });
+        }
     });
 });
 
